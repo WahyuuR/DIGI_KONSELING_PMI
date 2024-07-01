9bfe48a0cc46ab91e15379cb3d217b8f39d99c7f
WahyuuR <wahyu12rahman@gmail.com>
2024-07-01 15:06:51 +0700

    initial commit

--- middleware.js
+++ middleware.js
@@ -3,7 +3,6 @@ const express = require("express");
 const middleware = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static("public"));
 };
 
 module.exports = middleware;
