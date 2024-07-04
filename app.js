3e0eead87a50e857ccf2520d0616a80ae5cf2cfa
WahyuuR <wahyu12rahman@gmail.com>
2024-07-04 09:40:13 +0700

    initial commit

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const middleware = require("./src/middleware");
 const routes = require("./src/routes");
 const app = express();
-const speedInsights = require("@vercel/speed-insights");
-
-app.use(speedInsights.middleware());
 
 middleware(app);
 app.use(routes);
